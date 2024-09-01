74ba6ff618501c2d759766395669d7f81ba8efc5
Ibrahim Maaly <Jokerimxnz@gmail.com>
2024-09-01 16:36:12 +0200

    added database models

--- server.js
+++ server.js
@@ -3,3 +3,7 @@ const app = express()
 require("dotenv").config()
 
 app.use(express.json())
+
+
+const port = process.env.PORT || 7000
+app.listen(port, ()=>console.log(`listening to port ${port}`))
