45a5b3d8489e4f138827d0bc5afac22b09d5e3e8
Ibrahim Maaly <Jokerimxnz@gmail.com>
2024-09-02 16:31:04 +0200

    finished routes and controllers

--- server.js
+++ server.js
@@ -3,14 +3,17 @@ const app = express()
 const connectDB = require("./config")
 require("dotenv").config()
 const memberRouter = require('./Routes/memberRoute')
+const sectionRouter = require('./Routes/sectionRoute')
+const postRouter = require('./Routes/postRoute')
 
 connectDB()
 
 app.use(express.json())
-app.use(express.urlencoded({extended:false}))
+// app.use(express.urlencoded({extended:false}))
 
 app.use('/api/member', memberRouter)
-
+app.use('/api/section', sectionRouter)
+app.use('/api/post',postRouter)
 
 const port = process.env.PORT || 7000
 app.listen(port, ()=>console.log(`listening to port ${port}`))
