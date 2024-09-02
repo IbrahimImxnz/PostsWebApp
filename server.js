d61d10ad924cf83698922712dc3e3a0fb9c94f1a
Ibrahim Maaly <Jokerimxnz@gmail.com>
2024-09-02 15:33:14 +0200

    fixed errors and added member router to server

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require("express")
 const app = express()
 const connectDB = require("./config")
 require("dotenv").config()
-
+const memberRouter = require('./Routes/memberRoute')
 
 connectDB()
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
+
+app.use('/api/member', memberRouter)
 
 
 const port = process.env.PORT || 7000
