d6dbf45022883b7c7207b8d3181b0d4c8e782daf
Ibrahim Maaly <Jokerimxnz@gmail.com>
2024-09-04 16:15:31 +0200

    added middleware files

---
+++ validator.js
@@ -0,0 +1,11 @@
+const { validationResult } = require("express-validator");
+
+const validateError = (req, res, next) => {
+  const result = validationResult(req);
+  if (!result.isEmpty()) {
+    return res.status(400).json({ success: false, message: result.array() });
+  }
+  next();
+};
+
+module.exports = validateError;
